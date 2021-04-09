e2a614baec5a80f86f1f21a463dc3813697361b3
Anish <anishbudhathoki50@gmail.com>
2021-04-09 11:12:39 +0545

    3/9/2021

--- app/src/main/java/com/example/testhello/dashboard.java
+++ app/src/main/java/com/example/testhello/dashboard.java
@@ -3,12 +3,37 @@ package com.example.testhello;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class dashboard extends AppCompatActivity {
 
+    ListView lvInfoA;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        final String[] students = {"Birat","Prajanya","Pratik","Hari","Ram","Sam","Kiran","Rabin","Sandip","Bijay","Bipin","Torpey","Goktey",
+                        "Hero",
+                "Sabin",
+                "Birat","Prajanya","Pratik","Hari","Ram","Sam","Kiran","Rabin","Sandip","Bijay","Bipin","Torpey","Goktey",
+                "Hero",
+                "Sabin",
+        };
+        lvInfoA = (ListView) findViewById(R.id.lv_info_a);
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(dashboard.this,android.R.layout.simple_list_item_1,students);
+        lvInfoA.setAdapter(arrayAdapter);
+        lvInfoA.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(dashboard.this, students[position], Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/testhello/signUp.java
+++ app/src/main/java/com/example/testhello/signUp.java
@@ -3,8 +3,13 @@ package com.example.testhello;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class signUp extends AppCompatActivity {
+    EditText etName,etAddress,etEmail,etPhone,etPassword;
+    Button btnSignup;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
